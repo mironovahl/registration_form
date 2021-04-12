b5170e28f17641e18d11f84d9535e270b92967f3
Elena Mironova <mironovahl@yandex.ru>
2021-04-12 18:19:01 +0400

    fix: ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 
 import { store } from 'src/store'
 
-const MyApp = ({ Component, pageProps }: AppProps): ReactElement => (
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
-)
+const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
 
 export default MyApp
